99c002c9f378f4508d04a2f8fc0506119521cc8b
Juan Diego Marquez Chaparro <111793633+Lleidi159@users.noreply.github.com>
2022-11-01 01:04:36 -0700

    Clase 26 de oct del 2022

---
+++ 40 clases.cpp
@@ -0,0 +1,51 @@
+/***40 clases.cpp******
+// Curso Lenguaje C++ 
+// Programa: Demostración del uso de una clase, manejando datos de una agenda
+*****25 de oct 2022 Diego Marquez********/
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+class Registro 
+{ 
+	// Por omisión esto es la sección privada 
+		char nombre[50]; 
+		char telefono[15]; 
+	public: // Sección pública 
+ 		void Iniciar (void); 
+ 		void EntradaDatos (void); 
+ 		void SalidaDatos (void); 
+} registro1;
+ 
+// Programa principal 
+int main(void) 
+{ 
+	Registro registro2; 
+ 	registro1.Iniciar(); 
+ 	registro2.Iniciar(); 
+ 	//registro2.EntradaDatos();
+ 	registro1.EntradaDatos(); 
+ 	registro2.EntradaDatos(); 
+ 	registro1.SalidaDatos(); 
+ 	registro2.SalidaDatos(); 
+} 
+
+// Funciones miembro de la clase Registro 
+void Registro::Iniciar (void) 
+{ 
+	strcpy(nombre, ""); 
+ 	strcpy(telefono, ""); 
+} 
+
+void Registro::EntradaDatos (void) 
+{ 
+	cout << "\nNombre: "; 
+ 	cin >> nombre; 
+ 	cout << "Telefono: "; 
+ 	cin >> telefono; 
+} 
+void Registro::SalidaDatos (void) 
+{ 
+	cout << "\n\nNombre:\t\t" << nombre << "\n"; 
+	cout << "Telefono:\t" << telefono << "\n"; 
+} 
